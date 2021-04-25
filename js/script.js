7f88819aede59db93b33b9dc47bf6185c398cf6b
Tanitych <Tanitych@gmail.com>
2021-04-25 22:43:01 +0600

    fotter staid

--- js/script.js
+++ js/script.js
@@ -7,30 +7,109 @@ $(document).ready(function () {
     });
 });
 
+//swiper__slider
+const first__swipper = new Swiper('.first__swipper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    freeMode: false,
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 70,
+        stretch: 10,
+        slideShadows: true,
+    },
+    // autoplay: {
+    //     delay: 2000,
+    //     stopOnLastSlide: false,
+    // },
+    speed: 1000,
+});
 
-//slider
-$(document).ready(function () {
-    $('.slider').slick({
-        arrows: true,
-        dots: false,
-        slidesToShow: 1,
-        autoplay: false,
-        speed: 1000,
-        autoplaySpeed: 800,
+//service slider
+const swiper__service = new Swiper('.swiper__service', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 550,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+    navigation: {
+        nextEl: '.service_button_next',
+        prevEl: '.service_button_prev',
+    },
+    slidesPerView: 6,
+    spaceBetween: 16,
+    freeMode: true,
+    // autoplay: {
+    //     delay: 2000,
+    //     stopOnLastSlide: false,
+    // },
+    speed: 1000,
+    breakpoints: {
+        1300: {
+            slidesPerView: 6,
+        },
+        1100: {
+            slidesPerView: 5,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        550: {
+            slidesPerView: 3,
+        },
+        360: {
+            slidesPerView: 2,
+        },
+        315: {
+            slidesPerView: 1,
+        },
+    }
+});
+
+
+const news__slider = new Swiper('.news__slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+        nextEl: '.service_button_next',
+        prevEl: '.service_button_prev',
+    },
+    slidesPerView: 4,
+    spaceBetween: 15,
+    freeMode: true,
+    // autoplay: {
+    //     delay: 2000,
+    //     stopOnLastSlide: false,
+    // },
+    speed: 1000,
+    breakpoints: {
+        1000: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 13,
+        },
+        650: {
+            slidesPerView: 3,
+        },
+        365: {
+            slidesPerView: 2,
+        },
+        315: {
+            slidesPerView: 1,
+        },
+    }
 });
