eae0cc33dcf349dc378370b94a531f5d8b776e02
Tanitych <Tanitych@gmail.com>
2021-04-25 13:17:18 +0600

    chief doctor, reviews finished

--- js/script.js
+++ js/script.js
@@ -6,3 +6,31 @@ $(document).ready(function () {
         $('body').toggleClass('lock');
     });
 });
+
+
+//slider
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 1,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 800,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
