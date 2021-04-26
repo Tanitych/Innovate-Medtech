0e4158c7cc8e9161c996f8922293f7681fb14d74
Tanitych <Tanitych@gmail.com>
2021-04-26 23:20:44 +0600

    login page done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
     $('.header__burger, .header__link').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
+        $('.header__burger,.header__menu,.registration__header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
 });
