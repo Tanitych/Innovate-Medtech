aca870d7eab69de0a8239b8740953e0386802f40
Tanitych <Tanitych@gmail.com>
2021-05-02 12:22:48 +0600

    madal started

--- js/script.js
+++ js/script.js
@@ -209,7 +209,7 @@ tabsBtn.forEach(function (item) {
     });
 });
 
-document.querySelector('.tabs__nav__btn:nth-child(3)').click();
+document.querySelector('.tabs__nav__btn:nth-child(2)').click();
 
 //star rate
 
@@ -226,3 +226,30 @@ $(function () {
 
 
 });
+
+//modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
