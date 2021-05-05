badf67524f40f15c987cf5e781e0faf90f538073
Tanita <Tanitych@gmail.com>
2021-05-05 16:14:37 +0600

    doctor__profilee

--- js/script.js
+++ js/script.js
@@ -213,7 +213,7 @@ $(function () {
 var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("myBtn");
-
+var btn2 = document.getElementById("myBtn2");
 
 var span = document.getElementsByClassName("close")[0];
 
@@ -221,18 +221,29 @@ var span = document.getElementsByClassName("close")[0];
 btn.onclick = function () {
     modal.style.display = "block";
 }
-
-
 span.onclick = function () {
     modal.style.display = "none";
 }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
+
+btn2.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
 
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
 
 //subscribe 
 
